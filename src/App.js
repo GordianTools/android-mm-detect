9e50f78a95f9d4a4fbd5d46b9df6cc46d4033acc
Peter Conerly <pconerly@gmail.com>
2022-12-08 13:25:26 -0800

    try adding a hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { useCallback, useState} from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import detectEthereumProvider from '@metamask/detect-provider'
 import { ethers } from "ethers";
 
-const useProvider = (wndEth) => {
+const useDetectProvider = (wndEth) => {
   const [p, setp] = useState(null)
 
   useCallback(async () => {
@@ -15,6 +15,20 @@ const useProvider = (wndEth) => {
   return p
 }
 
+const useProvider = (wndEth) => {
+  const [p, setp] = useState(null)
+
+  useEffect(() => {
+    if (window.ethereum !== undefined) {
+    // console.log("wndethereum !== undefined");
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    setp(provider)
+    }}, [wndEth])
+
+  return p
+
+}
+
 function App() {
 
   // check window.ethereum repeatedly?
@@ -22,7 +36,7 @@ function App() {
   // useProvider
 
   // const detectedProvider = await detectEthereumProvider()
-  const detectedProvider = useProvider(window.ethereum);
+  const detectedProvider = useDetectProvider(window.ethereum);
   console.log('detectedProvider', detectedProvider)
 
   let provider;
@@ -31,6 +45,7 @@ function App() {
     provider = new ethers.providers.Web3Provider(window.ethereum, "any");
   }
 
+  const providerFromHook = useProvider(window.ethereum);
   
   return (
     <div className="App">
@@ -43,6 +58,8 @@ function App() {
         <p>Do we have window.etherem.isMetaMask? {!!wndEth.isMetaMask ? 'yes': 'no'} </p>
         <p>Detected Provider: {!!detectedProvider ? 'yes': 'no'}</p>
         <p>Provider via window: {!!provider ? 'yes': 'no'}</p>
+        <p>Provider via hook & window: {!!providerFromHook ? 'yes': 'no'}</p>
+        
       </header>
     </div>
   );
